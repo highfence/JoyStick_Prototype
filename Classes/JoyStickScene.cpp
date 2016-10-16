c70ae6707ca49195b33a32a87b3a896d78e2d825
highfence <highfence@naver.com>
2016-10-16 19:58:51 +0900

    Move by Arrows (Only once)

--- Classes/JoyStickScene.cpp
+++ Classes/JoyStickScene.cpp
@@ -3,6 +3,7 @@
 #include "SimpleAudioEngine.h"
 #include "base/CCEventListenerController.h"
 
+
 gainput::InputManager manager;
 gainput::InputMap map(manager);
 
@@ -32,7 +33,7 @@ bool HelloWorld::init()
                                            "CloseNormal.png",
                                            "CloseSelected.png",
                                            CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
-    
+
     closeItem->setPosition(Vec2(origin.x + visibleSize.width - closeItem->getContentSize().width/2 ,
                                 origin.y + closeItem->getContentSize().height/2));
 
@@ -40,11 +41,26 @@ bool HelloWorld::init()
     auto menu = Menu::create(closeItem, NULL);
     menu->setPosition(Vec2::ZERO);
     this->addChild(menu, 1);
-	
+
+	// Gamepad setting.
 	manager.SetDisplaySize(visibleSize.width, visibleSize.height);
 	const gainput::DeviceId padId = manager.CreateDevice<gainput::InputDevicePad>();
 	
-	map.MapBool(ButtonConfirm, padId, gainput::PadButtonA);
+	// Gamepad Key setting.
+	map.MapBool(ButtonExit, padId, gainput::PadButtonA);
+	map.MapBool(Left, padId, gainput::PadButtonLeft);
+	map.MapBool(Right, padId, gainput::PadButtonRight);
+	map.MapBool(Up, padId, gainput::PadButtonUp);
+	map.MapBool(Down, padId, gainput::PadButtonDown);
+	
+	// Sprite Setting.
+	m_pHead = Sprite::create(headFile);
+	m_pHead->setPosition(Vec2(visibleSize.width * INIT_HEAD_WIDTH,
+							  visibleSize.height * INIT_HEAD_HEIGHT));
+	addChild(m_pHead);
+
+
+	// Schedule Update.
 	scheduleUpdate();
     return true;
 }
@@ -66,6 +82,30 @@ void HelloWorld::update(float dt)
 	st += dt;
 	if (st >= 0.1f) {
 		manager.Update();
-		if (map.GetBoolWasDown(ButtonConfirm)) { exit(0); }
+		if (map.GetBoolWasDown(ButtonExit))
+		{
+			exit(0);
+		}
+		if (map.GetBoolIsNew(Left))
+		{
+			auto moveLeft = MoveBy::create(0, Vec2(-MOVE_SPEED, 0));
+			this->m_pHead->runAction(moveLeft);
+		}
+		else if (map.GetBoolIsNew(Right))
+		{
+			auto moveRight = MoveBy::create(0, Vec2(MOVE_SPEED, 0));
+			this->m_pHead->runAction(moveRight);
+		}
+		else if (map.GetBoolIsNew(Up))
+		{
+			auto moveUp = MoveBy::create(0, Vec2(0, MOVE_SPEED));
+			this->m_pHead->runAction(moveUp);
+		}
+		else if (map.GetBoolIsNew(Down))
+		{
+			auto moveDown = MoveBy::create(0, Vec2(0, -MOVE_SPEED));
+			this->m_pHead->runAction(moveDown);
+		}
+		st = 0.f;
 	}
 }
--- Classes/JoyStickScene.h
+++ Classes/JoyStickScene.h
@@ -2,8 +2,18 @@
 #define __HELLOWORLD_SCENE_H__
 
 enum Button {
-	ButtonConfirm
+	ButtonExit,
+	Left,
+	Right,
+	Up,
+	Down
 };
+
+const float INIT_HEAD_WIDTH = 0.5f;
+const float INIT_HEAD_HEIGHT = 0.5f;
+const int MOVE_SPEED = 10;
+
+const char headFile[] = "logo.png";
 
 class HelloWorld : public cocos2d::Layer
 {
@@ -14,6 +24,8 @@ public:
 	void update(float);
 
     void menuCloseCallback(cocos2d::Ref* pSender);
+
+	cocos2d::Sprite* m_pHead;
 
     // implement the "static create()" method manually
     CREATE_FUNC(HelloWorld);
